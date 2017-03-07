6462b1875d361e875b30c31c6ec9bd086ba06e14
Olga Coll Pérez <ocoll94@gmail.com>
2017-03-07 18:59:40 +0100

    Añadir ion-list en las características técnicas

--- src/pages/product/product.ts
+++ src/pages/product/product.ts
@@ -22,12 +22,33 @@ export class ProductPage {
   subtitle: string;
   description: HTMLElement;
   image: string;
+  info: string;
+  pReference:string;
+  pType : string;
+  pDimensions: string;
+  pConveyorWidth: string;
+  pConveyorLenght: string;
+  pConveyorEntry: string;
+  pVolume: string;
+  pWeight: string;
+  pPower: string;
+  pVoltage: string;
+  pFrequency: string;
+  pPrice: string;
+  pDetails: string;
+
+  pExtras = [];
+  pELabel: string;
+  pEReference: string;
+  pEPrice: string;
 
   constructor(public navCtrl: NavController, private http: Http, public params: NavParams, public loadingCtrl: LoadingController, public platform: Platform) {
     let loader = this.loadingCtrl.create({
       content: "Cargando...",
       duration: 1500
     });
+
+    this.info = "Características técnicas y PVP";
     loader.present();
     this.dataTaxonomyUrl = 'http://dosilet.deideasmarketing.solutions/wp-json/wp/v2/get_taxonomy_data_bycategory?idcategory=' + params.get("idCategory");
     this.dataProductUrl = 'http://dosilet.deideasmarketing.solutions/wp-json/wp/v2/get_products_bycategory?idcategory=' + params.get("idCategory");
@@ -50,7 +71,36 @@ export class ProductPage {
              && data[i].image.sizes!= "null"
              && data[i].image.sizes!= "undefined"
              && data[i].image.sizes.medium != "undefined") ? data[i].image.sizes.medium : "";
-          this.products.push({ id: data[i].idproduct, image: this.image, name: data[i].product.post_title });
+             this.products.push({ id: data[i].idproduct,
+                                  image: this.image,
+                                  name: data[i].product.post_title,
+                                  pReference : data[i].reference,
+                                  pType: data[i].type,
+                                  pDimensions: data[i].dimensions,
+                                  pConveyorWidth: data[i].conveyor_width,
+                                  pConveyorLenght: data[i].conveyor_length,
+                                  pConveyorEntry: data[i].conveyor_entry,
+                                  pVolume: data[i].volume,
+                                  pWeight: data[i].weight,
+                                  pPower: data[i].power,
+                                  pVoltage: data[i].voltage,
+                                  pFrequency: data[i].frequency,
+                                  pPrice: data[i].price,
+                                  pDetails: data[i].details})
+
+             this.pReference = data[i].reference;
+             this.pType = data[i].type;
+             this.pDimensions = data[i].dimensions;
+             this.pConveyorWidth = data[i].conveyor_width;
+             this.pConveyorLenght = data[i].conveyor_length;
+             this.pConveyorEntry = data[i].conveyor_entry;
+             this.pVolume = data[i].volume;
+             this.pWeight = data[i].weight;
+             this.pPower = data[i].power;
+             this.pVoltage = data[i].voltage;
+             this.pFrequency = data[i].frequency;
+             this.pPrice = data[i].price;
+             this.pDetails = data[i].details;
         }
         console.log(this.products);
       });
